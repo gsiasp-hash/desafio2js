7400efdadfc84f868834ff27e38b0319f9a831b4
Guillermo <g.siasp@gmail.com>
2026-04-03 14:40:49 -0300

    Add funcionalidad a la seccion stickers

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,22 @@ const planta = document.querySelector(".planta");
 planta.addEventListener("click", () => {
   planta.classList.toggle("borde");
 });
+
+const cantidad = document.querySelector(".cantidad");
+const sticker1 = document.getElementById("sticker1");
+const sticker2 = document.getElementById("sticker2");
+const sticker3 = document.getElementById("sticker3");
+const boton = document.getElementById("pedirStickers");
+
+boton.addEventListener("click", () => {
+  const cantidad1 = parseInt(sticker1.value) || 0;
+  const cantidad2 = parseInt(sticker2.value) || 0;
+  const cantidad3 = parseInt(sticker3.value) || 0;
+
+  const totalStickers = cantidad1 + cantidad2 + cantidad3;
+  if (totalStickers <= 10) {
+    cantidad.textContent = "Llevas " + totalStickers + " stickers.";
+  } else {
+    cantidad.textContent = "Llevas demasiados stickers.";
+  }
+});
